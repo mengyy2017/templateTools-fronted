150153b8399c6246b3432de0aa8059a258e4dda6
mengyy <243515205@qq.com>
2019-02-25 18:26:54 +0800

    1

---
+++ src/components/CodeFormInfo.js
@@ -0,0 +1,113 @@
+import React from "react";
+import connect from "react-redux/es/connect/connect";
+import {Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,} from 'antd';
+
+class CodeFormInfo extends React.Component{
+
+    next = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                this.props.changeActiveKey("1")
+            }
+        });
+    }
+
+    render = () => {
+
+        const { getFieldDecorator } = this.props.form;
+        // const {changeActiveKey} = this.props
+        
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 6 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 12 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+
+        return (
+            <div>
+
+                <Form onSubmit={this.next}>
+                    <Form.Item
+                        {...formItemLayout}
+                        label="database type"
+                    >
+                        {getFieldDecorator('databaseType', {
+                            rules: [
+                                { required: true, message: 'Please select your database type!' },
+                            ],
+                        })(
+                            <Select placeholder="Please select a database type">
+                                <Select.Option value="china">Mysql</Select.Option>
+                                <Select.Option value="usa">Oracle</Select.Option>
+                            </Select>
+                        )}
+                    </Form.Item>
+
+                    <Form.Item
+                        {...formItemLayout}
+                        label="database schema"
+                    >
+                        {getFieldDecorator('databaseSchema', {
+                            rules: [{ required: true, message: 'Please input your database schema!', whitespace: true }],
+                        })(
+                            <Input />
+                        )}
+                    </Form.Item>
+
+                    <Form.Item
+                        {...formItemLayout}
+                        label="code package"
+                    >
+                        {getFieldDecorator('codePackage', {
+                            rules: [{ required: true, message: 'Please input your code package!', whitespace: true }],
+                        })(
+                            <Input />
+                        )}
+                    </Form.Item>
+
+                    <Form.Item
+                        {...formItemLayout}
+                        label="author"
+                    >
+                        {getFieldDecorator('author', {
+                            rules: [{ required: false, message: 'Please input author!', whitespace: true }],
+                        })(
+                            <Input />
+                        )}
+                    </Form.Item>
+
+                    <Form.Item {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit">next</Button>
+                    </Form.Item>
+                </Form>
+
+            </div>
+        )
+    }
+
+}
+
+var mapStateToProps = state => ({ })
+
+const form = Form.create({ name: 'CodeFormInfo' })(CodeFormInfo);
+
+export default connect(mapStateToProps)(form)
--- src/components/ProcessTabs.js
+++ src/components/ProcessTabs.js
@@ -2,28 +2,32 @@ import {Tabs} from 'antd';
 import React from "react";
 import connect from "react-redux/es/connect/connect";
 import SelectDataTab from "components/SelectDataTab";
+import CodeFormInfo from "components/CodeFormInfo";
 const TabPane = Tabs.TabPane;
-
-
 
 class ProcessTabs extends React.Component{
 
-    callback = (key) => {
+    state = {
+            activeKey: "0",
+        }
+
+    changeActiveKey = (key) => {
         console.log(key);
+        this.setState({activeKey: key})
     }
 
     render = () => {
 
         return (
             <div>
-                <Tabs className="tabs-property" defaultActiveKey="1" onChange={this.callback}>
-                    <TabPane tab="Tab 1" key="1">
-                        Content of Tab Pane 1
+                <Tabs className="tabs-property" defaultActiveKey="1" activeKey={this.state.activeKey} onChange={this.changeActiveKey}>
+                    <TabPane disabled tab="Tab 0" key="0">
+                        <CodeFormInfo changeActiveKey={this.changeActiveKey.bind(this)}/>
                     </TabPane>
-                    <TabPane tab="Tab 2" key="2">
+                    <TabPane disabled tab="Tab 1" key="1">
                         <SelectDataTab/>
                     </TabPane>
-                    <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+                    <TabPane disabled tab="Tab 2" key="2">Content of Tab Pane 3</TabPane>
                 </Tabs>
             </div>
         )
