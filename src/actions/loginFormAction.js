f864e3197516bd25f6573a0765c2289a0e6270a8
mengyy <243515205@qq.com>
2019-03-13 18:37:12 +0800

    1

--- src/actions/loginFormAction.js
+++ src/actions/loginFormAction.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 // export const setCreateInfoToken = data => ({type: SET_CREATE_INFO_TOKEN, data})
 
-export const loginAction = data => async dispatch => {
+export const loginAction = data => dispatch => {
 
     // axios.get('http://127.0.0.1:8099/account/accDenied', {params: {
     //     username: 'Fred',
@@ -17,14 +17,28 @@ export const loginAction = data => async dispatch => {
     //         console.log(error);
     //     });
 
-    let response = await axios({
-        method: 'POST',
-        // url: 'http://127.0.0.1:8099/j_logiiiiii',
-        url: 'http://127.0.0.1:8099/account/accDenied',
-        headers: { 'content-type': 'application/json'},
-        data: data,
-        withCredentials: true
+    // let response = await axios({
+    //     method: 'POST',
+    //     // url: 'http://127.0.0.1:8099/j_logiiiiii',
+    //     url: 'http://127.0.0.1:8099/account/accDenied',
+    //     // headers: { 'Content-Type': 'application/json'},
+    //     // headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+    //     // headers: { 'Content-Type': 'application/json;charset=UTF-8'},
+    //     // data: JSON.stringify(data),
+    //     data: data,
+    //     withCredentials: true
+    // })
+
+    axios.post('http://127.0.0.1:8099/account/accDenied', {
+        firstName: 'Fred',
+        username: 'Flintstone'
     })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
     // console.log(response)
     // dispatch(getTablesSuccess(response.data))
