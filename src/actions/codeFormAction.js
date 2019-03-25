68ad236532d2784770e3684169ef447fd0b6941f
mengyy <243515205@qq.com>
2019-03-25 18:30:16 +0800

    1

--- src/actions/codeFormAction.js
+++ src/actions/codeFormAction.js
@@ -1,14 +1,24 @@
 import axios from 'axios'
 import {getTablesSuccess} from "actions/databaseAction";
+import {Redirect} from "react-router";
+import React from "react";
 
 export const setCodeInfoAction = pars => async dispatch => {
-    let {data} = await axios({
-        method: 'POST',
-        url: 'http://127.0.0.1:8099/database/setCreateInfo',
-        headers: { 'content-type': 'application/json'},
-        data: JSON.stringify(pars),
-        withCredentials: true
-    })
-    console.log(data)
-    dispatch(getTablesSuccess(data.respData))
+    try {
+        let {data} = await axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:8099/database/setCreateInfo',
+            headers: { 'content-type': 'application/json'},
+            data: JSON.stringify(pars),
+            withCredentials: true
+        })
+        dispatch(getTablesSuccess(data.respData))
+    } catch (e) {
+        if (e.response.status == "401") {
+            console.log(111111111111111111 + JSON.stringify(e))
+            return (<Redirect to="/login" />);
+        }
+    }
+    
+
 }
