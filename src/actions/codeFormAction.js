98c34b6d339a9385217a991aef0673ea5b2c24fe
mengyy <243515205@qq.com>
2019-03-26 13:29:23 +0800

    1

--- src/actions/codeFormAction.js
+++ src/actions/codeFormAction.js
@@ -1,23 +1,25 @@
 import axios from 'axios'
+import {message} from "antd";
 import {getTablesSuccess} from "actions/databaseAction";
 import React from "react";
 
 export const setCodeInfoAction = pars => async dispatch => {
-    try {
-        let {data} = await axios({
+        await axios({
             method: 'POST',
             url: 'http://127.0.0.1:8099/database/setCreateInfo',
             headers: { 'content-type': 'application/json'},
             data: JSON.stringify(pars),
             withCredentials: true
-        })
-        dispatch(getTablesSuccess(data.respData))
-    } catch (e) {
-        debugger
-        if (e.response.status == "401" && e.response.data.code == "401" && e.response.data.msg == "未认证") {
-            window.location.href = "http://127.0.0.1:8090/#/login"
-        }
-    }
-    
-
+        }).then(({data}) => dispatch(getTablesSuccess(data.respData)))
+            .catch(({response, response: {data}}) => {
+                if (response.status == "401" && data.code == "401" && data.msg == "未认证") {
+                    message.error(data.msg)
+                    window.location.href = "http://127.0.0.1:8090/#/login"
+                }
+            })
+    // {
+    //     xxx: "xxx",
+    //     response: {data: {msg: "未认证", code: "401"}},
+    //     yyy: "yyy"
+    // }
 }
