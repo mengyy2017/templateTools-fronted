cd41c112613df498337d4dee3af01b67dff94418
mengyy <243515205@qq.com>
2019-07-18 16:08:06 +0800

    1

--- src/components/spider/AutoCompletion.js
+++ src/components/spider/AutoCompletion.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {AutoComplete, Col} from 'antd';
+import {AutoComplete, Col, Select, Form} from 'antd';
 import {connect} from "react-redux";
 import {Row} from "antd";
 import {setAutoCompletionSourceAction} from "actions/spider/autoCompletionAction"
@@ -9,26 +9,64 @@ function onSelect(value) {
 }
 
 class AutoCompletion extends React.Component {
-    // state = {
-    //     dataSource: [],
-    // };
 
     handleSearch = phrase => {
-        // this.setState({
-        //     dataSource: !value ? [] : [value, value + "aaa", value + value + "bbb"],
-        // });
-        this.props.dispatch(setAutoCompletionSourceAction('indexName=wb&fieldName=school&phrase=' + phrase))
-    };
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                this.props.dispatch(setAutoCompletionSourceAction('indexName=wb&fieldName='+ values.fieldName +'&phrase=' + phrase))
+            }
+        });
 
-    onChange = () => {
-        alert(2)
-    }
+    };
 
     render = () => {
         const { AutoCompletionSource } = this.props;
+
+        const { getFieldDecorator } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 6 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+
         return (
             <Row>
-                <Col span={8} offset={8}>
+                <Col span={8} offset={1}>
+                    <Form>
+                        <Form.Item
+                            {...formItemLayout}
+                            label="search type"
+                        >
+                            {getFieldDecorator('fieldName', {
+                                initialValue: "school",
+                                rules: [
+                                    { required: true, message: 'Please select your search type!' },
+                                ],
+                            })(
+                                <Select placeholder="Please select a search type" size="large">
+                                    <Select.Option value="uName">姓名</Select.Option>
+                                    <Select.Option value="school">学校</Select.Option>
+                                    <Select.Option value="wbContent">微博内容</Select.Option>
+                                    <Select.Option value="summarize">简介</Select.Option>
+                                    <Select.Option value="address">所在地</Select.Option>
+                                    <Select.Option value="verify">认证</Select.Option>
+                                    <Select.Option value="tag">标签</Select.Option>
+                                    <Select.Option value="professional">职业</Select.Option>
+                                    <Select.Option value="commentText">评论</Select.Option>
+                                    <Select.Option value="commentUName">评论人</Select.Option>
+                                </Select>
+                            )}
+                        </Form.Item>
+
+                    </Form>
+                </Col>
+                <Col span={8}>
                     <AutoComplete dataSource={AutoCompletionSource} size="large"
                        style={{ width: 400 }} onSelect={onSelect}
                        onSearch={this.handleSearch} placeholder="input here"/>
@@ -42,4 +80,6 @@ class AutoCompletion extends React.Component {
 
 const mapStateToProps = state => ({AutoCompletionSource: state.autoCompletion ? state.autoCompletion.AutoCompletionSource : []})
 
-export default connect(mapStateToProps)(AutoCompletion)
+const AutoCompletionForm = Form.create({ name: 'AutoCompletion' })(AutoCompletion);
+
+export default connect(mapStateToProps)(AutoCompletionForm)
